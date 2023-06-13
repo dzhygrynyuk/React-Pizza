2e524d12be6ac8652864fefd2ce6d5777e9c78e0
dzhygrynyuk <dzhygrynyuk@gmail.com>
2023-06-13 21:37:02 +0300

    Created App functional component with useSelector, useDispatch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import {Routes, Route} from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
 
-import { connect } from 'react-redux';
 import { setPizzas } from './redux/actions/pizzas';
 
-/*
 function App() {
-  const [pizzas, setPizzas] = useState([]);
+  const dispatch = useDispatch();
+  const { items } = useSelector( ({ pizzas, filters }) => {
+    return {
+      items: pizzas.items,
+      sortBy: filters.sortBy
+    };
+  });
 
   useEffect(() => {
     axios.get('http://localhost:3001/pizzas').then(({data}) => {
-      setPizzas(data);
+      dispatch(setPizzas(data));
     });
   }, []);
 
@@ -25,7 +30,7 @@ function App() {
       <div className="content">
         <Routes>
           <Route exact path="/" element={
-            <Home pizzas={pizzas} />
+            <Home items={items} />
           } />
           <Route exact path="/cart" element={
             <Cart />
@@ -34,52 +39,6 @@ function App() {
       </div>
     </div>
   );
-} */
-
-class App extends React.Component {
-  componentDidMount() {
-    axios.get('http://localhost:3001/pizzas').then(({data}) => {
-      this.props.setPizzas(data);
-    });
-  }
-
-  render() {
-    console.log(this.props);
-    return(
-      <div className="wrapper">
-        <Header />
-
-        <div className="content">
-          <Routes>
-            <Route exact path="/" element={ 
-              <Home items={this.props.items}/> 
-            } />
-            <Route exact path="/cart" element={
-              <Cart />
-            } />
-          </Routes>
-        </div>
-      </div>
-    );
-  }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    items: state.pizzas.items,
-    filters: state.filters
-  };
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-    setPizzas: (items) => dispatch(setPizzas(items))
-  };
-}
-
-// OR Short Dispatch Variant
-// const mapDispatchToProps =  {
-//   setPizzas
-// }
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
