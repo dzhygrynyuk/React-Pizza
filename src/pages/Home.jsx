f1ecf674b515c2d7219b67dd484c639cdc4cdac5
dzhygrynyuk <dzhygrynyuk@gmail.com>
2023-06-17 00:44:32 +0300

    Creates CartItem component | Dispaly pizzes on Cart page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -57,7 +57,7 @@ function Home() {
                     ? items.map(obj => 
                         <PizzaBlock 
                             key={obj.id}
-                            addedCount={cartItems[obj.id] && cartItems[obj.id].length}
+                            addedCount={cartItems[obj.id] && cartItems[obj.id].items.length}
                             onAddToCart={onClickAddPizza}
                             {...obj}
                         />
