611b06203ed75155105a399956e0f3c90c86fa5e
dzhygrynyuk <dzhygrynyuk@gmail.com>
2023-06-17 23:10:48 +0300

    Clear Cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,16 +1,22 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import CartItem from "../components/CartItem";
+import { clearCart } from '../redux/actions/cart';
 
 import cartEmptyImg from '../assets/img/empty-cart.png';
 
 function Cart() {
+    const dispatch = useDispatch();
     const { items, totalPrice, totalCount } = useSelector( ({ cart }) => cart );
 
     const pizzasItems = Object.keys(items).map(key => {
         return items[key].items[0];
     });
+
+    const onClickClearCart = () => {
+        dispatch(clearCart());
+    }
 
     return (
         <div className="container container--cart">
@@ -25,14 +31,13 @@ function Cart() {
                             </svg>
                             Cart
                         </h2>
-                        <div className="cart__clear">
+                        <div onClick={onClickClearCart} className="cart__clear">
                             <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M2.5 5H4.16667H17.5" stroke="#B6B6B6" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"/>
                                 <path d="M6.66663 5.00001V3.33334C6.66663 2.89131 6.84222 2.46739 7.15478 2.15483C7.46734 1.84227 7.89127 1.66667 8.33329 1.66667H11.6666C12.1087 1.66667 12.5326 1.84227 12.8451 2.15483C13.1577 2.46739 13.3333 2.89131 13.3333 3.33334V5.00001M15.8333 5.00001V16.6667C15.8333 17.1087 15.6577 17.5326 15.3451 17.8452C15.0326 18.1577 14.6087 18.3333 14.1666 18.3333H5.83329C5.39127 18.3333 4.96734 18.1577 4.65478 17.8452C4.34222 17.5326 4.16663 17.1087 4.16663 16.6667V5.00001H15.8333Z" stroke="#B6B6B6" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"/>
                                 <path d="M8.33337 9.16667V14.1667" stroke="#B6B6B6" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"/>
                                 <path d="M11.6666 9.16667V14.1667" stroke="#B6B6B6" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"/>
                             </svg>
-
                             <span>Clear cart</span>
                         </div>
                     </div>
@@ -70,7 +75,7 @@ function Cart() {
                 </div>
             ) : (
                 <div className="cart cart--empty">
-                    <h2>Cart is empty <i>😕</i></h2>
+                    <h2>Cart is empty <icon>😕</icon></h2>
                     <p>You probably haven't ordered pizza yet.<br/> To order pizza, go to the main page.</p>
                     <img src={cartEmptyImg} alt="Empty cart" />
                     <Link to="/" className="button button--black">
